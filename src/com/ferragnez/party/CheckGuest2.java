4238bb6f5c583a2e64eba25f2d2cd1ecb0f47528
MatteInvi <matteoinvidia@gmail.com>
2025-04-17 19:54:11 +0200

    bonus

--- src/com/ferragnez/party/CheckGuest2.java
+++ src/com/ferragnez/party/CheckGuest2.java
@@ -2,9 +2,8 @@ package com.ferragnez.party;
 
 import java.util.Scanner;
 
-public class CheckGuest {
+public class CheckGuest2 {
     public static void main(String[] args) {
-
         String[] nomiInvitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti",
                 "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Tachel Zeilic" };
         Scanner scanner = new Scanner(System.in);
@@ -12,15 +11,13 @@ public class CheckGuest {
         System.out.println("Inserisci il tuo nome: ");
         String nomeRichiesto = scanner.nextLine().toUpperCase();
         boolean inLista = false;
-
-        for (int i = 0; i < nomiInvitati.length; i++) {
-            nomiInvitati[i] = nomiInvitati[i].toUpperCase();
-            if (nomeRichiesto.equals(nomiInvitati[i])) {
+        int i = 0;
+        while (inLista == false && i < nomiInvitati.length) {
+            if (nomeRichiesto.equals(nomiInvitati[i].toUpperCase())) {
                 inLista = true;
                 break;
-
             }
-
+            i++;
         }
 
         if (inLista == true) {
@@ -32,6 +29,4 @@ public class CheckGuest {
 
         scanner.close();
     }
-
-
 }
